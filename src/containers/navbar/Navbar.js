cba3d3385e73c14bc76d311660b481f115076697
David Huang <bugman195@gmail.com>
2019-05-18 00:59:38 +0800

    [container] Create Navbar

---
+++ src/containers/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import NavButton from '../../components/ui/buttons/NavButton';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavButton name="Home" />
+                <NavButton name="Resume" />
+                <NavButton name="Works" />
+                <NavButton name="Contact" />
+                <NavButton name="Podcasts" />
+                <NavButton name="Photos" />
+                <NavButton name="Design" />
+            </div>
+        );
+    }
+}
+
+export default Navbar;
