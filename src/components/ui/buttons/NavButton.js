9e30014111dad5514b06ef53b7149213b697a553
David Huang <bugman195@gmail.com>
2019-05-19 00:16:48 +0800

    [comp] Mod NavButton as NavLink & add CSS

--- src/components/ui/buttons/NavButton.js
+++ src/components/ui/buttons/NavButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavButton.module.css';
 
 const NavButton = (props) => {
     console.log(props);
     return (
-        <div className={classes.Button}>
-            <a href="#">{props.name}</a>
+        <div className={classes.Nav}>
+            <NavLink exact to={props.url} className={classes.Base} activeClassName={classes.Selected}>{props.name}</NavLink>
         </div>
     );
 }
