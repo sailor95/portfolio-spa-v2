3df41f93835779948158e73e51795de3fc0d4bef
David Huang <bugman195@gmail.com>
2019-05-19 00:13:29 +0800

    [container] Create DesignPage

---
+++ src/containers/navbar/design/DesignPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DesignPage extends React.Component {
+    render() {
+        return (
+            <div>
+                DesignPage
+            </div>
+        );
+    }
+}
+
+export default DesignPage;
