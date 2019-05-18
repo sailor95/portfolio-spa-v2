7e1d766aeb268c90983e2182b55fae22a1a303bd
David Huang <bugman195@gmail.com>
2019-05-19 00:14:20 +0800

    [comp] Add main nav routes

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Navbar from '../../containers/navbar/Navbar';
+import HomePage from '../../containers/navbar/home/HomePage';
+import ResumePage from '../../containers/navbar/resume/ResumePage';
+import WorksPage from '../../containers/navbar/works/WorksPage';
+import PodcastsPage from '../../containers/navbar/podcasts/PodcastsPage';
+import PhotosPage from '../../containers/navbar/photos/PhotosPage';
+import DesignPage from '../../containers/navbar/design/DesignPage';
 
 const Main = () => {
     return (
-        <Navbar />
+        <BrowserRouter>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/resume" component={ResumePage} />
+                <Route exact path="/works" component={WorksPage} />
+                <Route exact path="/podcasts" component={PodcastsPage} />
+                <Route exact path="/photos" component={PhotosPage} />
+                <Route exact path="/design" component={DesignPage} />
+            </Switch>
+        </BrowserRouter>
     );
 }
 
