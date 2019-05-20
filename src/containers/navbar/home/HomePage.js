ba461df30f927dd304d7549ace143da38ea22285
David Huang <david.huang@getac.com.tw>
2019-05-20 15:32:59 +0800

    [container] Temp data for design

--- src/containers/navbar/home/HomePage.js
+++ src/containers/navbar/home/HomePage.js
@@ -4,7 +4,14 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
-                Home
+                <h1>I am David Huang</h1>
+                <p>I love to design and make things, I value interface and UX.</p>
+                <p>My biggest dream is to develop a world changing product.</p>
+                <p>The idea of improving life through technology always brings me excitement and joy.</p>
+                <hr />
+                <p>linkedin | GitHub | Medium</p>
+                <br />
+                <p>This SPA is made by me, code & doc.</p>
             </div>
         );
     }
