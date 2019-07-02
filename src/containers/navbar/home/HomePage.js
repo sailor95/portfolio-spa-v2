42aee9252d45e65e2b997e6f1829c06f88524ff0
David Huang <bugman195@gmail.com>
2019-07-02 23:02:26 +0800

    [temp] Add Home page blueprint

--- src/containers/navbar/home/HomePage.js
+++ src/containers/navbar/home/HomePage.js
@@ -7,6 +7,10 @@ class HomePage extends React.Component {
         return (
             <div>
                 <IntroBlock />
+                <h5>Links block</h5>
+                <hr />
+                <h3>Skill Set</h3>
+                <h5>Skill blocks UI</h5>
                 <hr />
                 <p>Recent works</p>
                 <p>work 1 | work 2 | work 3</p>
