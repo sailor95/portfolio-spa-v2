b4867e7bfd2c3c1c5d40311eb1a5c9077f3a9d7f
David Huang <bugman195@gmail.com>
2019-07-13 20:13:53 +0800

    [util] Add Skill Set object

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -1,4 +1,62 @@
 import React from 'react';
-export let introString = (<p>I love to design and make things, I value the needs of user. <br />
+
+// Home page intro paragraph
+export const introString = (<p>I love to design and make things, I value the needs of user. <br />
     My dream is to develop world changing products that I put in my efforts. <br />
     I'm a hybrid geek who love arts, workout, business and the world of tech.</p>);
+
+// Home page skill set obj
+export const skillSet = {
+    "Front-end": [
+        {
+            tech: "React",
+            level: 5
+        },
+        {
+            tech: "CSS Module",
+            level: 4
+        },
+        {
+            tech: "Styled Component",
+            level: 4
+        },
+        {
+            tech: "SASS",
+            level: 4
+        },
+        {
+            tech: "Axure RP",
+            level: 4
+        }
+    ],
+    "Server-side": [
+        {
+            tech: "ASP.NET Core",
+            level: 5
+        },
+        {
+            tech: "Go",
+            level: 3
+        }
+    ],
+    "DB & DevOps": [
+        {
+            tech: "MSSQL",
+            level: 5
+        },
+        {
+            tech: "MongoDB",
+            level: 5
+        },
+        {
+            tech: "Azure",
+            level: 5
+        }
+    ],
+    "Others": [
+        {
+            tech: "Git",
+            level: 5
+        }
+    ]
+};
