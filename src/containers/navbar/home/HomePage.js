20c13a6d56584f058f3dc02cfa19d0687cf8f6a4
David Huang <bugman195@gmail.com>
2019-07-13 18:16:43 +0800

    [feature] Add SkillSetBlock and hr css

--- src/containers/navbar/home/HomePage.js
+++ src/containers/navbar/home/HomePage.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 import IntroBlock from '../../../components/ui/intro/IntroBlock';
 import { LinkBlock } from '../../../components/ui/links/LinkBlock';
+import { SkillSetBlock } from '../../../components/ui/blocks/blocks';
+import classes from './HomePage.module.css';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
+            <div className={classes.Layout}>
                 <IntroBlock />
                 <LinkBlock />
-                <hr />
-                <h3>Skill Set</h3>
-                <h5>Skill blocks UI</h5>
-                <hr />
+                <hr className={classes.Separator} />
+                <SkillSetBlock />
+                <hr className={classes.Separator} />
                 <p>Recent works</p>
                 <p>work 1 | work 2 | work 3</p>
-                <hr />
+                <hr className={classes.Separator} />
                 <p>linkedin | GitHub | Medium</p>
                 <br />
                 <p>This SPA is made by me, code & doc.</p>
