b5e3553a196d98b1b5f57c5c7407b51fca0aeca0
David Huang <bugman195@gmail.com>
2019-08-20 21:24:06 +0800

    [feature] Update about me

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 // Home page intro paragraph
 export const introString = (<p>I love to design and make things, I value the needs of user. <br />
-    My dream is to develop world changing products that I put in my efforts. <br />
-    I'm a hybrid geek who love arts, workout, business and the world of tech.</p>);
+    My dream is to develop world changing products with passion. <br />
+    I'm a hybrid geek who loves arts, workout, business and the world of tech.</p>);
 
 // Home page skill set obj
 export const skillSet = {
