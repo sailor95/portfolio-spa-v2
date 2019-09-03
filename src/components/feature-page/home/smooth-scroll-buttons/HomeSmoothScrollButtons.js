0700b9f256a3514cfcafc40c7cbad1c6a705ec9b
David Huang <bugman195@gmail.com>
2019-09-04 00:39:24 +0800

    [feature] Add react-scroll behavior

--- src/components/feature-page/home/smooth-scroll-buttons/HomeSmoothScrollButtons.js
+++ src/components/feature-page/home/smooth-scroll-buttons/HomeSmoothScrollButtons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 
 import NextSubjectIcon from '../../../../res/pictures/down-arrow-gray.png';
 import ToTopIcon from '../../../../res/pictures/up-arrow-gray.png';
@@ -6,23 +7,32 @@ import ToTopIcon from '../../../../res/pictures/up-arrow-gray.png';
 export const HomeNextSubjectButton = ({ subjectId, marginTop }) => {
     return (
         <div>
-            <img
-                src={NextSubjectIcon}
-                alt="Next"
-                style={{
-                    width: '3rem',
-                    height: '3rem',
-                    margin: `${marginTop} 1rem 1rem`,
-                    opacity: '30'
-                }}
-                title="To next" />
+            <ScrollLink
+                activeClass="active"
+                to={subjectId}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+            >
+                <img
+                    src={NextSubjectIcon}
+                    alt="Next"
+                    style={{
+                        width: '3rem',
+                        height: '3rem',
+                        margin: `${marginTop} 1rem 1rem`,
+                        opacity: '30'
+                    }}
+                    title="To next" />
+            </ScrollLink>
         </div>
     );
 };
 
-export const HomeToTopButton = ({ subjectId, marginTop }) => {
+export const HomeToTopButton = ({ marginTop }) => {
     return (
-        <div>
+        <div onClick={() => scroll.scrollToTop()}>
             <img
                 src={ToTopIcon}
                 alt="Next"
